adb4a55ccabad7db27e565022724fd2c0272009c
Anne-Marie Esteves <annemariee@free.fr>
2014-12-17 18:13:10 +0100

    correction du bug .to equal est plus restrictif que .to eq

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,10 +3,16 @@ require './spec/spec_helper'
 describe "fizz_buzz" do 
 
 	it "True donne true" do
-		expect(true).to equal (true)
+		expect(true).to eq(true)
 	end
 	it "3 donne fizz" do
-		expect(fizz_buzz(3)).to equal ("fizz")
+		expect(fizz_buzz(3)).to eq("fizz")
+	end
+	it "5 donne buzz" do
+		expect(fizz_buzz(5)).to eq("buzz")
+	end
+	it "15 donne fizz" do
+		expect(fizz_buzz(15)).to eq("fizzbuzz")
 	end
 
 end
