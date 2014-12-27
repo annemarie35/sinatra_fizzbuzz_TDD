e48e66835183049748c8c406801025a2bdbf74d7
Anne-Marie Esteves <annemariee@free.fr>
2014-12-27 18:39:41 +0100

    refactorisation de la machine roman to number

--- roman_to_nb.rb
+++ roman_to_nb.rb
@@ -1,19 +1,16 @@
 def roman_to_nb(roman)
 
 	romans = {"I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100, "M" => 1000}
-	roman = roman.to_s
-
-	romanlength = roman.length
-	return romans[roman] if romanlength == 1 
+	roman = roman.to_s #obligée de mettre ça pour la version sinatra
 
 	roman_inverse = roman.split('').reverse
 	number = romans[roman_inverse[0]]
 	
 	roman_inverse.each_with_index do |letter, indice| 
-	next_letter = roman_inverse[indice + 1] 
+		next_letter = roman_inverse[indice + 1] 
 
 		if romans[next_letter] == nil 
-				number
+			number
 
 		elsif romans[next_letter] < romans[letter]
 			number -= romans[next_letter]
