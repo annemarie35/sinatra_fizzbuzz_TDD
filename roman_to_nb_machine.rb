dfb56f05bf34131f4b8538eda5fae29322c08630
Anne-Marie Esteves <annemariee@free.fr>
2015-01-08 17:43:39 +0100

    correction pull review

--- roman_to_nb_machine.rb
+++ roman_to_nb_machine.rb
@@ -5,16 +5,16 @@ def roman_to_nb(roman)
 # if roman est un integer
 # renvoie le message Veuillez entrer un chiffre romain svp
 # elsif roman contient des lettres qui ne sont pas dans le dictionnaire romans
-# affiche veuillez entrer des chiffres romains comme I, V etc et ça bloque 
+# affiche veuillez entrer des chiffres romains comme I, V etc et ça bloque
 # elsif
 
 		roman_inverse = roman.to_s.split('').reverse
 		number = romans[roman_inverse[0]]
 		
-		roman_inverse.each_with_index do |letter, indice| 
-			next_letter = roman_inverse[indice + 1] 
+		roman_inverse.each_with_index do |letter, indice|
+			next_letter = roman_inverse[indice + 1]
 
-			if romans[next_letter] == nil 
+			if romans[next_letter] == nil
 				number
 
 			elsif romans[next_letter] < romans[letter]
@@ -24,7 +24,7 @@ def roman_to_nb(roman)
 				number += romans[next_letter]
 			end
 		 
-	   	end 
+	   	end
 		
 		number
 
