4afc839b11d11748a6f7fe51ae92c9c918a9b99e
Anne-Marie Esteves <annemariee@free.fr>
2015-02-04 16:36:28 +0100

    correction de la migration de la table notes qui causait un pb sur heroku et empechait le heroku rake db migrate de fonctionner

--- db/migrate/20150204092517_create_notes.rb
+++ db/migrate/20150204092517_create_notes.rb
@@ -3,7 +3,7 @@ class CreateNotes < ActiveRecord::Migration
     create_table :notes do |t|
       t.string :title
       t.text :body
-      t.timestamps
+      t.timestamps :date
     end
   end
 end
